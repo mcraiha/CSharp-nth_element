4f8779d96e0c09ddfaa6740d5c3ba5f3d31fc155
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-11-30 18:52:17 +0200

    Add simple test cases for both implementations
    In case someone wants to test them

---
+++ tests/tests-for-pd.cs
@@ -0,0 +1,25 @@
+namespace tests;
+using NthElementPD;
+
+public class TestsPD
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void SimpleTest()
+	{
+		// Arrange
+		int[] numberArray = new int[] { 12, 20, 16, -5, 8, 15, 19, -13, 11 }; // Sorted order -13 -5 8 11 12 15 16 19 20
+		int[] copyArray = (int[])numberArray.Clone();
+
+		// Act
+		PartialSort.nth_element(array: numberArray, startIndex: 0, nthSmallest: 4, endIndex: numberArray.Length - 1);
+
+		// Assert
+		Assert.AreEqual(11, numberArray[3]);
+		CollectionAssert.AreNotEqual(numberArray, copyArray);
+	}
+}
